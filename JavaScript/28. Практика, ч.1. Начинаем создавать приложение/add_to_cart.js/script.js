18daeae7510ed5d93b093a95e17f0cb294583e3b
AtabekIskandarov <77529344+AtabekIskandarov@users.noreply.github.com>
2021-04-03 22:24:47 +0300

    init

---
+++ JavaScript/28. Практика, ч.1. Начинаем создавать приложение/add_to_cart.js/script.js
@@ -0,0 +1,23 @@
+let ttl = document.getElementsByClassName("ttl");
+console.log(ttl[0].innerText);
+
+
+// let desc_img = document.getElementsByClassName("desc_img");
+// console.log(desc_img[0]);
+
+let img = document.getElementsByTagName('img');
+console.log(img[1]);
+
+//  let price = document.getElementsByClassName("price");
+//  console.log(price[0]);
+
+let span = document.getElementsByTagName('span');
+console.log(span[14].innerText);
+
+let del = document.getElementsByTagName("del");
+console.log(del[0].innerText);
+
+
+ 
+
+
