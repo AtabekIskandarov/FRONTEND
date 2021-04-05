2e2088c55feef7a9a64a2e878bcb9bd94fa5ff40
AtabekIskandarov <77529344+AtabekIskandarov@users.noreply.github.com>
2021-04-05 22:42:20 +0300

    init

--- JavaScript/28. Практика, ч.1. Начинаем создавать приложение/sripts.js
+++ JavaScript/28. Практика, ч.1. Начинаем создавать приложение/sripts.js
@@ -77,10 +77,28 @@
 
 
 
-   if (2==2){
-    alert("ОТВЕТ ВЕРНЫЙ");
-   } else {
-    alert("ОТВЕТ НЕВЕРНЫЙ!");
-   }
+//    if (2==2){
+//     alert("ОТВЕТ ВЕРНЫЙ");
+//    } else {
+//     alert("ОТВЕТ НЕВЕРНЫЙ!");
+//    }
 
-
+   let ttl = document.getElementsByClassName("ttl");
+   console.log(ttl[0].innerText);
+   
+   
+   // let desc_img = document.getElementsByClassName("desc_img");
+   // console.log(desc_img[0]);
+   
+   let img = document.getElementsByTagName('img');
+   console.log(img[1]);
+   
+   //  let price = document.getElementsByClassName("price");
+   //  console.log(price[0]);
+   
+   let span = document.getElementsByTagName('span');
+   console.log(span[14].innerText);
+   
+   let del = document.getElementsByTagName("del");
+   console.log(del[0].innerText);
+   
